79d80b5eb83ec17058178f639f58751e7ec159de
sakanaction317 <danki.karate9602@gmail.com>
2024-11-06 23:45:23 +0900

    平均評価を表示させよう

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-bottom mt-5">
+    <a href="{{ url('/') }}" class="navbar-brand mx-auto">
+        <img src="{{ asset('img/logo.jpg') }}" class="img-fluid">
+    </a>
+</nav>
---
+++ resources/views/users/favorite.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex justify-content-center mt-3">
+    <div class="w-75">
+        <h1>お気に入り</h1>
+
+        <hr>
+
+        <div class="row">
+            @foreach($favorite_products as $favorite_product)
+                <div class="col-md-7 mt-2">
+                    <div class="d-inline-flex">
+                        <a href="{{ route('products.show', $favorite_product->id) }}" class="w-25">
+                            @if($favorite_product->image !== "")
+                            <img src="{{ asset($favorite_product->image) }}" class="img-fluid w-100">
+                            @else
+                            <img src="{{ asset('img/dummy.png') }}" class="img-fluid w-100">
+                            @endif
+                        </a>
+                        <div class="container mt-3">
+                            <h5 class="w-100 samuraimart-favorite-item-text">{{ $favorite_product->name }}</h5>
+                            <h6 class="w-100 samuraimart-favorite-item-text">{{ $favorite_product->price }}</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex align-items-center justify-content-end">
+                    <a href="{{ route('favorites.destroy', $favorite_product->id) }}" class="samuraimart-favorite-item-delete" onclick="event.preventDefault(); document.getElementById('favorites-destroy-form{{ $favorite_product->id }}'.submit();)">
+                        削除
+                    </a>
+                    <form action="{{ route('favorites.destroy', $favorite_product->id) }}" class="d-none" id="favorites-destroy-form{{ $favorite_product->id }}" method=""POST>
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </div>
+                <div class="col-md-3 d-flex align-items-center justify-content-end">
+                    <form action="{{ route('carts.store') }}" method="post" class="m-3 align-items-end">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $favorite_product->id }}">
+                        <input type="hidden" name="name" value="{{ $favorite_product->name }}">
+                        <input type="hidden" name="price" value="{{ $favorite_product->price }}">
+                        <input type="hidden" name="image" value="{{ $favorite_product->image }}">
+                        <input type="hidden" name="carriage" value="{{ $favorite_product->carriage_flag }}">
+                        <input type="hidden" name="qty" value="1">
+                        <input type="hidden" name="weight" value="0">
+                        <button class="btn samuraimart-favorite-add-cart">
+                        カートに入れる
+                    </button>
+                    </from>
+                </div>
+                @endforeach
+        </div>
+
+        <hr>
+    </div>
+</div>
+@endsection
